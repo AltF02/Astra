e3c11d168873e2ee69ca939b5a5136fb5f3122ca
DankDumpster <staalkoper@gmail.com>
2021-01-08 05:58:53 +0100

    removed deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-TARGET=x86_64-unknown-linux-musl
-
-cargo build --release --target $TARGET
-ssh azure 'pm2 stop astra'
-scp -r ./target/$TARGET/release/astra azure:/home/noodles/astra/astra
-#ssh azure 'pm2 start astra'
